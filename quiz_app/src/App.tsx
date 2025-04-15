3fccb6e24f5e4e53e274037466de0fc2293db390
anassazz <anass.azz2030@gmail.com>
2025-04-15 15:35:08 +0100

    first commit

---
+++ quiz_app/src/App.tsx
@@ -0,0 +1,20 @@
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Quiz from './pages/Quiz';
+import Result from './pages/Result';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
