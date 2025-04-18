9ec85492e86fae070d7a01c3ae2bd6980ee54404
anassazz <anass.azz2030@gmail.com>
2025-04-18 09:30:43 +0100

    refactor Home component layout for improved readability

--- quiz_app/src/pages/Home.tsx
+++ quiz_app/src/pages/Home.tsx
@@ -14,21 +14,24 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <div className='flex flex-col border-2 p-3 rounded-md'>
+      <img className='w-80 mb-3' src="https://st2.depositphotos.com/3837271/9109/i/450/depositphotos_91090308-stock-photo-wooden-blocks-with-the-text.jpg" alt="quiz" />
         
-      <input
-        type="text"
-        placeholder="Entrez votre nom"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        className="border p-2 rounded mb-4"
-      />
-      <button
-        onClick={handleStart}
-        className="bg-orange-500 text-white px-4 py-2 rounded"
-      >
-        Commencer le Quiz
-      </button>
+        <input
+          type="text"
+          placeholder="Entrez votre nom"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="border p-2 rounded mb-4"
+        />
+        <button
+          onClick={handleStart}
+          className="bg-orange-500 text-white px-4 py-2 rounded"
+        >
+          Commencer le Quiz
+        </button>
+      </div>
     </div>
   );
 };
