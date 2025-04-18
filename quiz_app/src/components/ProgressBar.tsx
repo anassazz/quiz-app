8378a7859467539792ef03b2b7d5a92a978933ea
anassazz <anass.azz2030@gmail.com>
2025-04-18 09:14:16 +0100

    add ProgressBar component to visualize quiz progress

---
+++ quiz_app/src/components/ProgressBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface Props {
+  current: number;
+  total: number;
+}
+
+const ProgressBar: React.FC<Props> = ({ current, total }) => {
+  const percent = (current / total) * 100;
+  return (
+    <div className="w-full bg-gray-300 rounded-full h-4">
+      <div className="bg-blue-500 h-4 rounded-full" style={{ width: `${percent}%` }}></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
